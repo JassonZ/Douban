24dfe20ede57786fd4b12281af11eaea2f0d0397
张羽 <zy614125682@gmail.com>
2019-05-20 16:25:11 +0800

    创建flutter项目

---
+++ douban/lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:douban/pages/splash_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main(){
+  runApp(MyApp());
+  if(Platform.isAndroid){
+    //设置Android头部的导航栏透明
+    SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  } 
+}
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return RestartWidget(
+      child: MaterialApp(
+          theme: ThemeData(backgroundColor: Colors.white),
+          home: Scaffold(
+            resizeToAvoidBottomPadding: false,
+            body: SplashWidget(),
+            ),
+      ),
+    );
+  }
+}
+
+class RestartWidget extends StatefulWidget{
+  final Widget child;
+
+  RestartWidget({Key key,@required this.child})
+      :assert(child != null),
+      super(key: key);
+  static restartApp(BuildContext context){
+    final _RestartWidgetState state = context.ancestorStateOfType(const TypeMatcher<_RestartWidgetState>());
+    state.restartApp();
+  }
+
+  @override 
+  _RestartWidgetState createState() => _RestartWidgetState();
+}
+
+class _RestartWidgetState extends State<RestartWidget>{
+  Key key = UniqueKey();
+  void restartApp(){
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
+  @override 
+  Widget build(BuildContext context){
+    return Container(
+      key: key,
+      child: widget.child,
+      );
+  }
+}
---
+++ douban/lib/pages/container_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+///这个页面是作为整个APP的最外层的容器，以Tab为基础控制每个item的显示和隐藏
+class ContainerPage extends StatefulWidget {
+  ContainerPage({Key key}) : super(key: key);
+
+  @override 
+  State<StatefulWidget> createState(){
+    return null;
+  }
+}
